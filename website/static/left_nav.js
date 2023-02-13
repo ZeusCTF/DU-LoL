8cb003e6e28099768b3321a56c9e61ffa4826239
Acquiesc <alee38@email.davenport.edu>
2023-02-13 16:11:50 -0500

    Display classes, mobile nav
    main classes (main.css): show-on-mobile, show-on-tablet, show-on-desktop
    	show-on-mobile - display: block < 768px
    	show-on-tablet - display: block 768px - 1022px
    	show-on-desktop - display: block > 1022px
    
    created left nav menu for mobile display, opened and closed using left_nav.js
    
    added root colors

---
+++ website/static/left_nav.js
@@ -0,0 +1,8 @@
+
+function toggleLeftNav() {
+    let leftNav = document.querySelector(".main-left-nav");
+
+    leftNav.classList.toggle("show-from-left");
+
+    console.log("Toggled hide left on mobile nav menu");
+}
