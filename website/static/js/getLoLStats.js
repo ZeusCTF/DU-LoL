e2da3619ad6d315c5eb3709613253ad9396f391d
Acquiesc <alee38@email.davenport.edu>
2023-02-16 13:01:08 -0500

    RiotAPI Initial Commit
    created getLoLStats.js & basic functionality
    
    added container boxes to fill in LoL.html stats placeholder

---
+++ website/static/js/getLoLStats.js
@@ -0,0 +1,29 @@
+const statContainer = document.querySelector(".container-profile-stats");
+
+const API_KEY = "RGAPI-64bf4914-2bce-4b32-a667-8ae44e058d6a";
+
+const playerIGN = document.getElementById("ign").innerHTML;
+
+async function getPlayer() {
+
+    //set up API call
+    const APICallString = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + playerIGN + "?api_key=" + API_KEY;
+
+    const response = await fetch(APICallString);
+    let data = await response.json();
+    console.log(data);
+
+    //handle API call
+
+    //set summoner icon
+    let sumIconImg = document.getElementById("sumIcon");
+    sumIconImg.src = ("http://ddragon.leagueoflegends.com/cdn/13.3.1/img/profileicon/" + data.profileIconId + ".png");
+
+    //set summoner name
+    let sumName = document.getElementById("sumName");
+    sumName.innerHTML = data.name;
+    
+    //set summoner level
+    let sumLevel = document.getElementById("sumLevel");
+    sumLevel.innerHTML = data.summonerLevel;
+}
