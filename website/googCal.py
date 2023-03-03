0e4be14d87cb226d8f1a0a59e4f3632650d8e0ce
Acquiesc <alee38@email.davenport.edu>
2023-03-03 16:53:05 -0500

    Calendar Event Display/Create
    Pulled more info from calendar events
    
    Sent more info for calendar events from schedule.html form
    
    Fixed display bugs with calendar
    
    Added filler profile image

--- website/googCal.py
+++ website/googCal.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-
+from datetime import datetime
 
 import os.path
 
@@ -22,21 +22,39 @@ def pullEvent():
     page_token = None
     totalEvents = list()
 
-    
-
     while True:
+
         events = service.events().list(calendarId='primary', pageToken=page_token).execute()
         for event in events['items']:
+            #this format 2023-02-16T17:00:00-00:00
+            startDateTime = event['start'].get('dateTime')
+            startDateTime = str(startDateTime)
+
+            startTime = startDateTime[11:16]
+            startDate = startDateTime[0:10]
+
+            endDateTime = event['end'].get('dateTime')
+            endDateTime = str(endDateTime)
+
+            endTime = endDateTime[11:16]
+            endDate = endDateTime[0:10]
+
             eventInfo = {
                 "summary": (event['summary']),
-                "startDate": event['start'].get('date')
+                "startDate": startDate,
+                "startTime": startTime,
+                "endDate": endDate,
+                "endTime": endTime,
+                "desc": (event['description']),
+                "location": (event['location']),
+                "url": (event['htmlLink'])
             }
             totalEvents.append(eventInfo)
         page_token = events.get('nextPageToken')
         if not page_token:
             return totalEvents
         
-def addEvent(summary, startTime):
+def addEvent(summary, startDate, endDate, startTime, endTime, location, eventDetails):
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
     # If there are no (valid) credentials available, let the user log in.
@@ -45,22 +63,33 @@ def addEvent(summary, startTime):
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                '/Users/barryallen/CodingProjects/DU-LoL/website/credentials.json', SCOPES)
+                'C:/xampp/htdocs/flask testing/website/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
     try:
+        #build dateTime strings
+        startDateTime = f'{startDate}' + "T" + f'{startTime}' + ":00-00:00"
+        print("Start Date Time String")
+        print(startDateTime)
+
+        endDateTime = f'{endDate}' + "T" + f'{endTime}' + ":00-00:00"
+        print("End Date Time String")
+        print(endDateTime)
+
         service = build('calendar', 'v3', credentials=creds)
         event = {
             'summary': f'{summary}',
+            'description': f'{eventDetails}',
+            'location': f'{location}',
             'start': {
-                'date': f'{startTime}'
+                #this format 2023-02-16T17:00:00-00:00
+                'dateTime': startDateTime,
             },
             'end': {
-                            #this format 2023-02-16T17:00:00-07:00
-                'date': '2999-02-17'
+                'dateTime': endDateTime
             }}
 
         event = service.events().insert(calendarId='primary', body=event).execute()
