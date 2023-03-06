d8df24722b75dce9883f3d1f7b3d6a10aeaf0633
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2023-03-06 16:01:19 -0500

    Update YouTubeAPI.py

--- website/YouTubeAPI.py
+++ website/YouTubeAPI.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import pprint
 
 
 def gatherVideos():
@@ -21,4 +22,36 @@ def gatherVideos():
         except:
             break
     return vidLinks
+#gatherVideos()
+
+def gatherVideos():
+    r = requests.get('https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCTHMPvX_wADl18GLtYRxjUQ&maxResults=10&order=date&key=AIzaSyB297x7ugH4cce5TBt1C_uGOLep7kXKCk0')
+    ytURL = 'https://www.youtube.com/watch?v='
+
+    vidLinks = []
+
+
+    resp = r.json()
+
+    x = json.dumps(resp)
+    y = json.loads(x)
+
+    for num in range(0,11):
+        try:
+            #print(y["items"][num]['id']['videoId'])
+            vidLinks.append(ytURL + str(y["items"][num]['id']['videoId']))
+        except:
+            break
+    #print(vidLinks)
+
+    #json response but pretty
+    #pprint.pprint(y)
+
+    #thumbnail urls
+    #print(y["items"][0]['snippet']['thumbnails']['default']['url'])
+
+    #title
+    #print(y["items"][0]['snippet']['title'])
+
+
 gatherVideos()
