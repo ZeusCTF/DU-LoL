e011abec31ede3a5dbfaaad6389b964239f06732
Acquiesc <alee38@email.davenport.edu>
2023-03-07 12:57:29 -0500

    Youtube API Search Query
    Updated search query to playlistItem instead of standard search > reduce quota from 100 to 1 per call.
    
    adjusted python list return in correspondence with new API json return
    
    Searches for max of 50 videos in playlist

--- website/YouTubeAPI.py
+++ website/YouTubeAPI.py
@@ -3,41 +3,42 @@ import json
 import pprint
 
 def gatherVideos():
-    r = requests.get('https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCTHMPvX_wADl18GLtYRxjUQ&maxResults=3&order=date&key=AIzaSyB297x7ugH4cce5TBt1C_uGOLep7kXKCk0')
+    r = requests.get('https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLUzkJoFm5OFqgVgJ16DrwJp8GdEn5Ezgn&maxResults=3&order=date&key=AIzaSyB297x7ugH4cce5TBt1C_uGOLep7kXKCk0')
     ytURL = 'https://www.youtube.com/watch?v='
 
     vidLinks = []
-#UCTHMPvX_wADl18GLtYRxjUQ - personal
+#UCTHMPvX_wADl18GLtYRxjUQ - project
 #UCaxSQhSboBlHxBkPJOpCvTQ - DU
     resp = r.json()
 
     x = json.dumps(resp)
     y = json.loads(x)
 
-    for num in range(0,11):
+    pprint.pprint(y)
+
+    for num in range(0,50):
         try:
+            #thumbnail urls
+            print(y["items"][num]['snippet']['thumbnails']['high']['url'])
+            #title
+            print(y["items"][num]['snippet']['title'])
+            #videoSrc
+            print(str(y["items"][num]['snippet']['resourceId']['videoId']))
 
             video = {
                 "thumbURL": y["items"][num]['snippet']['thumbnails']['high']['url'],
                 "title": y["items"][num]['snippet']['title'],
-                "videoSRC": ytURL + str(y["items"][num]['id']['videoId'])
+                "videoSRC": ytURL + str(y["items"][num]['snippet']['resourceId']['videoId'])
             }
 
+            print("Video found:")
+            print(video)
+
             vidLinks.append(video)
-
-            pprint.pprint(y)
-
-            #print(y["items"][num]['id']['videoId'])
-            #vidLinks.append(ytURL + str(y["items"][num]['id']['videoId']))
-
-            #thumbnail urls
-            #print(y["items"][num]['snippet']['thumbnails']['default']['url'])
-
-            #title
-            #print(y["items"][num]['snippet']['title'])
         except:
-            break
+            print("error finding video")
     
+    print("Found the following videos:")
     print(vidLinks)
     return vidLinks
 
