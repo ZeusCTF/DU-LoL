538e095678cb897b8fae731da73405ed9b8ab80b
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2023-03-09 10:58:48 -0500

    Quick updates

--- website/YouTubeAPI.py
+++ website/YouTubeAPI.py
@@ -3,7 +3,7 @@ import json
 import pprint
 
 def gatherVideos():
-    r = requests.get('https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLUzkJoFm5OFqgVgJ16DrwJp8GdEn5Ezgn&maxResults=3&order=date&key=AIzaSyB297x7ugH4cce5TBt1C_uGOLep7kXKCk0')
+    r = requests.get('https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLUzkJoFm5OFqgVgJ16DrwJp8GdEn5Ezgn&maxResults=10&order=date&key=AIzaSyB297x7ugH4cce5TBt1C_uGOLep7kXKCk0')
     ytURL = 'https://www.youtube.com/watch?v='
 
     vidLinks = []
@@ -14,17 +14,19 @@ def gatherVideos():
     x = json.dumps(resp)
     y = json.loads(x)
 
-    pprint.pprint(y)
+    #pprint.pprint(y)
 
-    for num in range(0,50):
+    #max results is set to 10 in the api call, adjusting range to match
+    for num in range(0,10):
         try:
             #thumbnail urls
-            print(y["items"][num]['snippet']['thumbnails']['high']['url'])
+            #print(y["items"][num]['snippet']['thumbnails']['high']['url'])
             #title
-            print(y["items"][num]['snippet']['title'])
+            #print(y["items"][num]['snippet']['title'])
             #videoSrc
-            print(str(y["items"][num]['snippet']['resourceId']['videoId']))
+            #print(str(y["items"][num]['snippet']['resourceId']['videoId']))
 
+        #creates a 'video' dictionary
             video = {
                 "thumbURL": y["items"][num]['snippet']['thumbnails']['high']['url'],
                 "title": y["items"][num]['snippet']['title'],
@@ -36,10 +38,17 @@ def gatherVideos():
 
             vidLinks.append(video)
         except:
-            print("error finding video")
+            pass
+            #I'm not sure this is needed, I figured the necessary amoount of boxes would be returned, and if there weren't any videos to be found, then they just wouldn't show
+            #we can talk about this though
+
+            #print("error finding video, or video does not exist")
     
+    #I think this is kinda redundant, if you are already printing the video dictionaries above
     print("Found the following videos:")
-    print(vidLinks)
+    for vid in vidLinks:
+        print(vid['title'])
+    
     return vidLinks
 
     
